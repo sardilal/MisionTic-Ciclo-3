c82b369a85f7a68b82faf0946306a5961885dc11
Sardilal <ardilaloaizasebastian@gmail.com>
2022-09-13 19:25:01 -0500

    Añado los archivos de inicio de Spring

---
+++ src/main/java/com/misiontic/misiontic/TicEnviosApplication.java
@@ -0,0 +1,13 @@
+package com.misiontic.misiontic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicEnviosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicEnviosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/misiontic/misiontic/TicEnviosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misiontic.misiontic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicEnviosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
