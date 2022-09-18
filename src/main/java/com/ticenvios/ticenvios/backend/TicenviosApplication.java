ce260b74ab89a3f77269178ba61ef6e24e12a3cd
Sardilal <ardilaloaizasebastian@gmail.com>
2022-09-17 22:56:09 -0500

    Se realiza la configuración de los controladores para el index y se hacen pruebas para establecer controladores futuros

--- src/main/java/com/ticenvios/ticenvios/backend/TicenviosApplication.java
+++ src/main/java/com/ticenvios/ticenvios/backend/TicenviosApplication.java
@@ -1,4 +1,4 @@
-package com.ticenvios.ticenvios;
+package com.ticenvios.ticenvios.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
